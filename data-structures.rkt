5ad7b30759b4ec889e6e90adb9810e1fe04e6c58
Florian Biermann <fbie@itu.dk>
2016-01-12 12:02:41 +0100

    extend and update script

--- data-structures.rkt
+++ data-structures.rkt
@@ -33,19 +33,16 @@
 ;; accessible in the body of the let binding. This is comparable to
 ;; the scope of a variable in Java, where you would say int x2 = 2 * x;:
 
-(: double-and-add (-> Real Real Real)) ;; The type of the function can
-                                       ;; be written like this, above
-                                       ;; the "define" line. -> means
-                                       ;; function, the last type in
-                                       ;; the list is the return type,
-                                       ;; the others are parameter
-                                       ;; types.
+;; The type of the function can be written like this, above the
+;; "define" line. -> means function, the last type in the list is the
+;; return type, the others are parameter types.
+(: double-and-add (-> Real Real Real))
 (define (double-and-add x y)
-  (let ([x2 (* 2 x)]  ;; Compute 2 * x and bind it to x2.
-        [y2 (* 2 y)]) ;; Compute 2 * y and bind it to y2.
-    (+ x2 y2)))       ;; Add the two results.
+  (let ([x2 (* 2 x)]               ;; Compute 2 * x and bind it to x2.
+        [y2 (* 2 y)])              ;; Compute 2 * y and bind it to y2.
+    (+ x2 y2)))                    ;; Add the two results.
 
-(double-and-add 42 34)
+(double-and-add 42 23)
 
 ;; In functional programming languages, state is immutable. If you
 ;; bind a value to a name via let, you are not allowed to change it
@@ -81,7 +78,8 @@
 
 (struct None ())           ;; This struct contains noting.
 (struct (A) Some ([a : A]) ;; This struct contains one value of type
-                           ;; A. Types must be inlined for structs.
+                           ;; A. Types must be inlined for structs. It
+                           ;; is like a generic class in Java.
   #:transparent)
 
 ;; (#:transparent means that we can see what's inside the instance
@@ -182,17 +180,62 @@
 (ann (Cons 5 (Cons 3 (Nil))) (LinkedList Integer))
 ;(ann (Cons "what" (Cons 5 (Cons 3 (Nil)))) (LinkedList Integer))
 
+;; We might want to know, how many elements are there in the list,
+;; just as in Java, where you can use arr.length.[
+
 ;; In functional programming languages, state is immutable. Therefore,
 ;; we cannot use for-loops with counters as you know them from Java or
 ;; C. Instead, we need to use recursion to build functions that
-;; operate on the recursive decomposition of a data
-;; structure. Consider the function get-first. It returns Some first
-;; element of the list, or None:
+;; operate on the "recursive decomposition" of a data
+;; structure. Length is a polymorphic function. You can compare it to
+;; a generic method in Java.
+(: Length (All (A) (-> (LinkedList A) Integer)))
+(define (Length as)
+  (match as
+    [(Nil) 0]
+    [(Cons _ tail) (+ 1 (Length tail))]))
 
-(require racket/match)
+(Length (Cons 'a (Cons 'b (Cons 'c (Nil)))))
 
-;; get-first is a polymorphic function. You can compare it to a
-;; generic method in Java.
+;; Maybe we have two lists that we want to add to each other. That
+;; happens often. So let's look at how to write an append function for
+;; to lists.
+;;
+;; Q: How would you go about doing that? What are the invariants?
+
+(: Append (All (A) (-> (LinkedList A) (LinkedList A) (LinkedList A))))
+(define (Append lhs rhs)
+  (match lhs
+    [(Nil) rhs] ;; Nothing to do, because the left list is empty.
+    [(Cons a tail) (Cons a (Append tail rhs))])) ;; Put the current element in the beginning and recurse.
+
+(Append
+ (Cons 'a (Cons 'b (Nil)))
+ (Cons 'x (Cons 'y (Cons 'z (Nil)))))
+
+;; Q: Do you start seeing a pattern?
+;;
+;; It seems that we always have to look at two cases:
+;; - Nil, for when the list is empty. Here, we stop and return a default value.
+;; - Cons, for when there is at least one element in the list left. We
+;;     handle the element and then we proceed to the next element of the
+;;     list, which might be either Nil or again Cons.
+
+;; Q: Implement a function that reverses the order of elements in a
+;; list, such that (Reverse (Cons 'a (Cons 'b (Nil)))) == (Cons 'b
+;; (Cons 'a (Nil))). Hint: You may use Append here.
+
+(: Reverse (All (A) (-> (LinkedList A) (LinkedList A))))
+(define (Reverse as)
+  (match as
+    [(Nil) (Nil)]
+    [(Cons a tail) (Append (Reverse tail) (Cons a (Nil)))]))
+
+(Reverse (Cons 'a (Cons 'b (Cons 'c (Nil)))))
+
+;; Consider the function get-first. It returns Some first element of
+;; the list, or None:
+
 (: get-first (All (A) (-> (LinkedList A) (Maybe A))))
 (define (get-first as)
   (match as
@@ -251,18 +294,22 @@
 ;; list. Say you have a list of grades for students, but you graded
 ;; them too low. Everyone should get 10% more. So you write:
 
-(struct Student ([name : String] [grade : Number]) #:transparent)
-(define-type StudentList (LinkedList Student))
+(struct Student ([name : String] [grade : Real]) #:transparent)
 
-(: everyone-better-grade (-> StudentList StudentList)) ;; Remember, you cannot change the old list.
+(: better-grade (-> Student Student))
+(define (better-grade s)
+  (Student (Student-name s) (+ (Student-grade s) 10))) ;; Increase current grade by ten.
+
+;; Remember, you cannot change the old list.
+(: everyone-better-grade (-> (LinkedList Student) (LinkedList Student)))
 (define (everyone-better-grade ss)
   (match ss
     [(Nil) (Nil)] ;; End of list, nothing to do.
     [(Cons s tail) (Cons
-                    (Student (Student-name s) (+ (Student-grade s) 10)) ;; Change the current student's grade.
+                    (better-grade s) ;; Change the current student's grade.
                     (everyone-better-grade tail))])) ;; Continue to the next.
 
-(: students StudentList)
+(: students (LinkedList Student))
 (define students (Cons (Student "Li" 34)
                        (Cons (Student "Wu" 80)
                              (Cons (Student "Xiaoning" 73)
@@ -273,5 +320,115 @@
 (everyone-better-grade students)
 
 ;; Okay, that was easy. But you also have a bunch of books that you
-;; want to sell on the internet. But you want to lower your price by
-;; 15RMB per book. Is that not a similar problem?
+;; want to sell on the internet. You have the author and the title and
+;; want to know how much they cost when purchased new. So you go:
+
+(struct Book ([title : String] [author : String]))
+
+(: get-book-price (-> Book Number))
+(define (get-book-price b)
+  (+ 100 (random 500))) ;; The mighty book price oracle has spoken.
+
+(: get-book-prices (-> (LinkedList Book) (LinkedList Number)))
+(define (get-book-prices bs)
+  (match bs
+    [(Nil) (Nil)]
+    [(Cons b tail) (Cons (get-book-price b) (get-book-prices tail))]))
+
+(: books (LinkedList Book))
+(define books (Cons (Book "Spreadsheet Implementation Technology" "Peter Sestoft")
+                    (Cons (Book "Java Concurrency in Practice" "Brian Goetz")
+                          (Cons (Book "Computers and Intractability" "Michael Garey and David Johnson")
+                                (Nil)))))
+
+(get-book-prices books)
+
+;; Do you see, what happened? We have defined two functions that
+;; operate on lists and perform an action on each of the lists
+;; elements. The functions look very similar: either, we have Nil and
+;; the list is empty. Or, we have Cons, so there is at least one more
+;; element which we can apply the function to.
+;;
+;; Q: Do you think we can generalize this pattern? If so, how?
+;;
+;; In functional programming, there are some well known higher-order
+;; functions that abstract such repeating patterns. One of these is
+;; Map.
+
+(: Map (All (A B) (-> (-> A B) (LinkedList A) (LinkedList B))))
+
+;; Map takes a function of type (-> A B) and a list of As and then
+;; returns a list of Bs. This means that it applies the function to
+;; each element of the original list and returns a new list. That is
+;; exactly what we want, so let's take a look at how to do that.
+
+(define (Map f as)
+  (match as
+    [(Nil) (Nil)]
+    [(Cons a tail) (Cons (f a) (Map f tail))]))
+
+;; And now we repeat the operations, using the smaller functions on
+;; single (sometimes also called scalar) values as parameter:
+
+(Map better-grade students)
+(Map get-book-price books)
+
+;; Now we have learned that we can apply any function on single
+;; elements to a collection of elements, such as a linked list.
+
+;; Let us look further at lists. We might want to remove an element
+;; from a list (i.e. build a new list without that element) if some
+;; predicate holds. Can we generalize that, too? Of course!
+
+(: Filter (All (A) (-> (-> A Boolean) (LinkedList A) (LinkedList A))))
+(define (Filter p? as)
+  (match as
+    [(Nil) (Nil)]
+    [(Cons a tail) (let ([t (Filter p? tail)])
+                     (if (p? a)
+                         t
+                         (Cons a t)))]))
+
+(Filter (lambda ([s : Student]) (< (Student-grade s) 40)) students)
+
+;; Project:
+;;
+;; Here is a Typed-Racket implementation of BinaryTree. You may draw
+;; inspiration from the Java examples.
+
+(define-type (BinaryTree A) (U (Leaf A) (Node A (BinaryTree A) (BinaryTree A))))
+(struct (A) Leaf ([a : A]))
+;; The type variable name must be named exactly as many times as it
+;; occurrs in the definition.
+(struct (A A A) Node ([a : A] [lhs : (BinaryTree A)] [rhs : (BinaryTree A)]))
+
+;; 1) Write a function Size that returns the number of elements in a
+;; binary tree instance. Explain, in English, what your function does
+;; in each step.
+
+(: Size (All (A) (-> (BinaryTree A) Integer)))
+(define (Size ba)
+  ;; NOT to be handed out.
+  (match ba
+    [(Leaf _) 1]
+    [(Node _ lhs rhs) (+ 1 (Size lhs) (Size rhs))]))
+
+;; 2) Write a function Invert that swaps the left-hand side and the
+;; right-hand side of a binary tree instance. Explain, in English,
+;; what your function does in each step.
+
+(: Invert (All (A) (-> (BinaryTree A) (BinaryTree A))))
+(define (Invert ba)
+  (match ba
+    [(Leaf _) ba]
+    [(Node a lhs rhs) (Node a (Invert rhs) (Invert lhs))]))
+
+;; 3) Write a function MapTree that maps a function f to all elements
+;; of some binary tree instance. Explain, in English, what your
+;; function does in each step.
+
+(: MapTree (All (A B) (-> (-> A B) (BinaryTree A) (BinaryTree B))))
+(define (MapTree f ba)
+  (match ba
+    [(Leaf a) (Leaf (f a))]
+    [(Node a lhs rhs) (Node (f a) (MapTree f lhs) (MapTree f rhs))]))
