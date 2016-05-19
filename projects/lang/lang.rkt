4c432abeb9741d2ac36c847e0c4a88b478dc3928
Florian Biermann <fbie@itu.dk>
2016-05-19 09:21:22 +0800

    add annoated version of matt might's interpreter

--- projects/lang/lang.rkt
+++ projects/lang/lang.rkt
@@ -1,62 +1,14 @@
-#lang typed/racket
+#lang racket
 
-;; This is our Term type, which models the abstract syntax tree.
-(define-type Term (U Var Abs App Closure))
-(struct Var ([s : String]) #:transparent) ;; A variable name.
-(struct Abs ([v : Var] [body : Term]) #:transparent) ;; A function. Functions are abstractions.
-(struct App ([f : Term] [e : Term]) #:transparent) ;; Function application, f(x) in Java or (f x) in Racket.
-
-;; Parse an expression in lambda calculus into an Term.
-(: parse (-> Any Term))
-;; You do not have to understand how this works exactly. It is enough
-;; if you see that we have some syntax on the left which we convert
-;; into Term types on the right. If the syntax is not correct, we
-;; throw an exception.
-(define (parse expr)
-  (match expr
-    [(? symbol?)     (Var (format "~a" expr))]
-    [`(λ ,v . ,body) (Abs (Var (format "~a" v)) (parse body))]
-    [`(,f ,e)        (App (parse f) (parse e))]
-    [_               (error (string-append (format "Illegal expression: \"~a\"" expr)))]))
-
-;; The environment is a list of pairs of symbols to Term.
-(define-type Env (Listof (Pairof Var Term)))
-
-;; Retrieve the value stored for the variable name.
-(: env-lookup (-> Var Env Term))
-(define (env-lookup s env)
-  (let ([v (assq s env)])
-    (if (not (eq? v #f))
-        (cdr v)
-        (error (format "Variable not bound to a value: ~a" s)))))
-
-;; Store a value under a variable name.
-(: env-push (-> Var Term Env Env))
-(define (env-push sym term env)
-  (cons (cons sym term) env))
-
-;; A closure is an anonymous function that encloses an environment.
-(struct Closure ([v : Var] [body : Term] [env : Env]) #:transparent)
-
-;; Evaluation interprets a Term without compilation.
-(: eval (-> Term Env Term))
 (define (eval term env)
-  (match term
-    [(Var s)      (env-lookup term env)] ;; Look up the value of the symbol in the environment.
-    [(Abs v body) (Closure v body env)]  ;; Produce a closure: a function that encloses an environment.
-    [(App f e)    (closure-apply (eval f env) e)])) ;; Call the closure on the parameter expression e.
+  (match term ;; Case decomposition on the term structure:
+    [(? symbol?) (cadr (assq term env))] ;; Look-up symbol in the environment.
+    [`(λ ,x . ,f) `(closure ,term ,env)] ;; Produce a new closure that encloses an environment.
+    [`(,f ,x) (apply (eval f env) (eval x env))])) ;; Evaluate f and apply its result to the result of x.
 
-;; Apply a closure to a term.
-(: closure-apply (-> Term Term Term))
-(define (closure-apply closure e)
-  (match closure
-    ;; We evaluate a closure with the enclosed environment in which
-    ;; the variable name v has been bound to the value of e, which is
-    ;; an already evaluated term:
-    [(Closure v body env) (eval body (env-push v e env))]
-    [_                    (error "Can only apply closures.")]))
+(define (apply f x)
+  (match f ;; Case decomposition on the function structure.
+    [`(closure (λ ,v . ,body) ,env) ;; We only accept closures here!
+         (eval body (cons `(,v ,x) env))])) ;; Evaluate the body with an environment where v is bound to x.
 
-;; Parse a lambda calculus term from command line.
-(: parse-from-cmd (-> Term))
-(define (parse-from-cmd)
-  (parse (read)))
+(display (eval (read) '())) ;; Print the result of evaluating a parsed expression.
